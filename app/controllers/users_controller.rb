f94d9d79ca6a0175f45efe76778037182bd5dcf8
阿部裕紀 <hiroki-abe@K04hrk-b.local>
2016-02-10 10:48:16 +0900

    add edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
-
+  before_action :set_current_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,11 +20,23 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
 
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location)
+  end
+
+  def set_current_user
+    @user = User.find(current_user.id)
   end
 end
